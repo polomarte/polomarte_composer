3e3f9c3f9af8dbe58d8af019fbef14aa822bc525
Theo Bittencourt <theodrums@gmail.com>
2014-12-05 17:05:39 -0200

    Remove deprected 'treat_symbols_as_metadata_keys_with_true_values' method from spec_helper

--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -33,7 +33,6 @@ RSpec.configure do |config|
 
   config.filter_run wip: true
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 
   ActiveRecord::Migration.maintain_test_schema!
 
